04d4c06f98b7b85db18ddff1118e5a49d1c6619c
David Eriksson <david@2good.nu>
2016-12-26 12:37:56 +0100

    Rebuild inside "build" subdirectory

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -e
 set -x
 export CFLAGS="-Wall -Werror -ggdb3"
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/var/tmp/unshield . && make && make install
+cd `dirname $0`
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/var/tmp/unshield .. && make && make install
