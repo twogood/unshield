e2c0e005465c34afff72c526714a66c242008130
wdlkmpx <wdlkmpx@gmail.com>
2021-05-10 17:24:25 +0800

    libunshield: always use the internal md5.c
    remove references to openssl

--- lib/file.c
+++ lib/file.c
@@ -1,10 +1,6 @@
 /* $Id$ */
 #include "internal.h"
-#if USE_OUR_OWN_MD5
 #include "md5/md5.h"
-#else
-#include <openssl/md5.h>
-#endif
 #define MD5Init   MD5_Init
 #define MD5Update MD5_Update
 #define MD5Final  MD5_Final
