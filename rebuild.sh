2f2de24c8a5e6ad5d008cbebc5952f00f1c6df88
wdlkmpx <wdlkmpx@gmail.com>
2021-05-16 22:56:42 +0800

    rebuild.sh (windows): CFLAGS=-W3 (avoid MSVC nonsensical warnings)

--- rebuild.sh
+++ rebuild.sh
@@ -4,13 +4,15 @@ set -x
 
 if test "$TRAVIS_OS_NAME" = "windows" ; then
     # see .travis.yml
-    export CFLAGS="-Wall"
     if test "$TRAVIS_COMPILER" = "clang" ; then
         # https://clang.llvm.org/docs/MSVCCompatibility.html
         # clang on Windows uses MSVC stuff but it's not recognize but CMakeLists
         # so must use MSVC instead, just unset some vars
         echo "*** Will use MSVC ***"
+        export CFLAGS="-W3"
         unset CC CC_FOR_BUILD CXX CXX_FOR_BUILD
+    else
+        export CFLAGS="-Wall" # gcc
     fi
     .github/travis_windows_build.bat
     exit $?
