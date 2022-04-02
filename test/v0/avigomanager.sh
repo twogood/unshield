5f4aad42a71c9f82423cd1983fef14045b83caff
David Eriksson <david@activout.se>
2022-04-02 13:09:19 +0200

    git lfs test

--- test/v0/avigomanager.sh
+++ test/v0/avigomanager.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 set -e
-cd `dirname $0`
+cd $(dirname "$0")
 MD5_FILE=`pwd`/`basename $0 .sh`.md5
+ZIP_FILE=`pwd`/`basename $0 .sh`.zip
 UNSHIELD=${UNSHIELD:-/var/tmp/unshield/bin/unshield}
 
-if [ \! -x ${UNSHIELD} ]; then
+if [ \! -x "${UNSHIELD}" ]; then
     echo "unshield executable not found at $UNSHIELD" >&2
     exit 1
 fi
 
-DIR=`mktemp -d`
+DIR=$(mktemp -d)
 trap 'rm -rf ${DIR}' TERM INT EXIT
-cd ${DIR}
+cd "${DIR}"
 
-#URL=https://www.ti.com/organizers/avigo/docs/avigomanager11b22.zip
-URL="https://www.dropbox.com/s/8r4b6752swe3nhu/unshield-avigomanager11b22.zip?dl=1"
-curl -sSL -o test.zip ${URL}
-unzip -q test.zip 'data*'
+if [ \! -e "$ZIP_FILE" ]; then
+	#URL=https://www.ti.com/organizers/avigo/docs/avigomanager11b22.zip
+	URL="https://www.dropbox.com/s/8r4b6752swe3nhu/unshield-avigomanager11b22.zip?dl=1"
+	curl -sSL -o "$ZIP_FILE" "${URL}"
+fi
+unzip -q -d "$DIR" "$ZIP_FILE" 'data*'
+
 
 set +e
-timeout 10 ${UNSHIELD} -d extract1 x data1.cab > log1 2>&1
+timeout 10 "${UNSHIELD}" -d extract1 x data1.cab > log1 2>&1
 CODE=$?
 if [ ${CODE} -ne 1 ]; then
     cat log1 >&2
@@ -27,7 +31,7 @@ if [ ${CODE} -ne 1 ]; then
     exit 2
 fi
 
-timeout 10 ${UNSHIELD} -O -d extract2 x data1.cab > log2 2>&1
+timeout 10 "${UNSHIELD}" -O -d extract2 x data1.cab > log2 2>&1
 CODE=$?
 if [ ${CODE} -ne 0 ]; then
     cat log2 >&2
@@ -37,7 +41,7 @@ fi
 
 cd extract2
 find . -type f -print0 | LC_ALL=C sort -z | xargs -0 md5sum > ../md5
-if ! diff -wu ${MD5_FILE} ../md5 >&2 ; then
+if ! diff -wu "${MD5_FILE}" ../md5 >&2 ; then
     echo "MD5 sums diff" >&2
     exit 4
 fi
