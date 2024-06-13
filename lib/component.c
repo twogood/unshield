a3f54a1741f6944d99ea7537d3c7ba6aaa64f938
Cosmin Tanislav <demonsingur@gmail.com>
2024-06-13 03:30:27 +0300

    Do not abort if MAX_FILE_GROUP_COUNT exceeded
    MAX_FILE_GROUP_COUNT limits the number of file group offset
    lists inside the CabDescriptor.
    
    Components do not have the same limitations, as the file group
    count is specified in the header.

--- lib/component.c
+++ lib/component.c
@@ -49,9 +49,6 @@ UnshieldComponent* unshield_component_new(Header* header, uint32_t offset)
   }
 
   self->file_group_count = READ_UINT16(p); p += 2;
-  if (self->file_group_count > MAX_FILE_GROUP_COUNT)
-    abort();
-
   self->file_group_names = NEW(const char*, self->file_group_count);
 
   file_group_table_offset = READ_UINT32(p); p += 4;
